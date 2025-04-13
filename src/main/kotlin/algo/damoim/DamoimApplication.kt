832223a84c8cf6ac3f1ed688c1995ec2b5330b09
dlqudtjs <dlqudtjs922@gmail.com>
2025-04-13 15:58:54 +0900

    first commit

---
+++ src/main/kotlin/algo/damoim/DamoimApplication.kt
@@ -0,0 +1,11 @@
+package algo.damoim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DamoimApplication
+
+fun main(args: Array<String>) {
+    runApplication<DamoimApplication>(*args)
+}
