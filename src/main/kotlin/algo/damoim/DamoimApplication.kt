b37d08a87716b0f3890b557f4e7d2b4752a6e063
dlqudtjs <dlqudtjs922@gmail.com>
2025-04-20 17:51:58 +0900

    feat(setting): 환경변수 설정

--- src/main/kotlin/algo/damoim/DamoimApplication.kt
+++ src/main/kotlin/algo/damoim/DamoimApplication.kt
@@ -7,5 +7,10 @@ import org.springframework.boot.runApplication
 class DamoimApplication
 
 fun main(args: Array<String>) {
+    println("=== ENV DUMP BEFORE SPRING START ===")
+    println("DB_URL      = ${System.getenv("DB_URL")}")
+    println("DB_USERNAME = ${System.getenv("DB_USERNAME")}")
+     println("DB_PASSWORD = ${System.getenv("DB_PASSWORD")}")
+    println("====================================")
     runApplication<DamoimApplication>(*args)
 }
